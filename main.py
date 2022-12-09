bc1fb5e5d9e23d31be7cc04607da03f1191e5204
Tarlykov D <tarlykov@gmail.com>
2022-12-10 00:29:14 +0300

    Update commit

--- main.py
+++ main.py
@@ -1,23 +1,47 @@
-with open('recipes.txt', encoding='utf-8') as f:
-    cook_book = []
-    for line in f:
-        cook_book_name = line.strip()
-        ingredients_count = int(f.readline())
-        ingredients = []
-        for i in range(ingredients_count):
-            ingredient = f.readline().strip()
-            name, quantity, measure = ingredient.split(' | ')
-            ingredients.append({
-                'ingredient_name': name,
-                'quantity': quantity,
-                'measure': measure
-            })
-        f.readline()
-        cook_book.append({
-            'employees_count': ingredients_count,
-            'employees': ingredients
-        })
-        cook_book_names = {cook_book_name: cook_book}
+from pprint import pprint
+def dict_collector(file_path):
+    with open(file_path, 'r',encoding ='utf 8' ) as file_work:
+        menu = {}
+        for line in file_work:
+            dish_name = line[:-1]
+            counter = file_work.readline().strip()
+            list_of_ingridient = []
+            for i in range(int(counter)):
+                dish_items = dict.fromkeys(['ingredient_name', 'quantity', 'measure'])
+                ingridient = file_work.readline().strip().split(' | ')
+                for item in ingridient:
+                    dish_items['ingredient_name'] = ingridient[0]
+                    dish_items['quantity'] = ingridient[1]
+                    dish_items['measure'] = ingridient[2]
+                list_of_ingridient.append(dish_items)
+                cook_book = {dish_name: list_of_ingridient}
+                menu.update(cook_book)
+            file_work.readline()
 
-print(cook_book_names)
-print(type(cook_book_names))
+    return(menu)
+
+def get_shop_list_by_dishes(dishes, persons=int):
+    menu = dict_collector('recipes.txt')
+    print('Меню ресторана:')
+    pprint(menu)
+    print()
+    shopping_list = {}
+    try:
+        for dish in dishes:
+            for item in (menu[dish]):
+                items_list = dict([(item['ingredient_name'], {'measure': item['measure'], 'quantity': int(item['quantity'])*persons})])
+
+                if shopping_list.get(item['ingredient_name']):
+                    extra_item = (int(shopping_list[item['ingredient_name']]['quantity']) + int(items_list[item['ingredient_name']]['quantity']))
+                    shopping_list[item['ingredient_name']]['quantity'] = extra_item
+
+                else:
+                    shopping_list.update(items_list)
+
+        print(f"Чтобы приготовить блюдо на {persons} персон нужно купить:")
+        pprint(shopping_list)
+    except KeyError:
+        print("Нет таких блюд")
+
+dict_collector('recipes.txt')
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
